2b0b63a0b562239ecc75379b7b51018347eedd31
chemuu933 <j02711659@gmail.com>
2025-09-13 14:28:43 +0300

    small changes

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  }
+  plugins: [vue()],
 })
